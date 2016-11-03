530b916c69ba4a0e7c0c9331a2028f10ead95509
Ravenm <ravenmetals@gmail.com>
2016-11-03 12:20:55 -0500

    Add files via upload

---
+++ readerWriter.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace temp
+{
+    partial class readerWriter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.outPutBox = new System.Windows.Forms.TextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(282, 146);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(75, 23);
+            this.openBtn.TabIndex = 0;
+            this.openBtn.Text = "Open File";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // outPutBox
+            // 
+            this.outPutBox.Location = new System.Drawing.Point(58, 81);
+            this.outPutBox.Multiline = true;
+            this.outPutBox.Name = "outPutBox";
+            this.outPutBox.Size = new System.Drawing.Size(176, 228);
+            this.outPutBox.TabIndex = 1;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(282, 176);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 2;
+            this.saveBtn.Text = "Write File";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtm_Click);
+            // 
+            // readerWriter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 337);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.outPutBox);
+            this.Controls.Add(this.openBtn);
+            this.Name = "readerWriter";
+            this.Text = "Read/Write";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.TextBox outPutBox;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
+
---
+++ readerWriter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+///Name:
+///Name:
+namespace temp
+{
+    public partial class readerWriter : Form
+    {
+        public readerWriter()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> nameList = new List<string>();
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void readFileByLine()
+        {
+            FileStream fs = File.Open(openFileDialog1.FileName, FileMode.OpenOrCreate);
+            BufferedStream bs = new BufferedStream(fs);
+            StreamReader sr = new StreamReader(bs);
+            var lineOfFile = "";
+            while((lineOfFile = sr.ReadLine()) != null)
+            {
+                nameList.Add(lineOfFile);
+            }
+            nameList.Sort();
+            sr.Close();
+            bs.Close();
+            fs.Close();
+        }
+
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            StringBuilder stringBuidler = new StringBuilder();
+            openFileDialog1.ShowDialog();
+            try
+            {
+                readFileByLine();
+                
+                foreach (var element in nameList)
+                {
+                    stringBuidler.AppendLine(element);
+                }
+            }
+            catch(IOException)
+            {
+
+            }
+            outPutBox.Text = stringBuidler.ToString();
+        }
+
+        private void saveBtm_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.Create);
+            BufferedStream bs = new BufferedStream(fs);
+            StreamWriter streamWritter = new StreamWriter(bs);
+            foreach (var element in nameList)
+            {
+                streamWritter.WriteLine(element);
+            }
+            streamWritter.Close();
+            bs.Close();
+            fs.Close();
+        }
+    }
+}
